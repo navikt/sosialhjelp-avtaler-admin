10e18ce865cbcf9638308b1d7d499b04409d4451
Martin Almvik <martin.almvik@nav.no>
2024-06-03 18:19:53 +0200

    Hail mary

--- src/pages/api/avtalemal/[...slug].ts
+++ src/pages/api/avtalemal/[...slug].ts
@@ -1,6 +1,31 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { getOboToken, withAuth } from "@/auth/withAuth";
-import { proxyApiRouteRequest } from "@navikt/next-api-proxy";
+import { IncomingHttpHeaders } from "http";
+import { Stream } from "stream";
+import http, { RequestOptions } from "http";
+import { Readable } from "node:stream";
+
+export async function stream2buffer(stream: Stream): Promise<Buffer> {
+  return new Promise<Buffer>((resolve, reject) => {
+    const buffer = Array<Uint8Array>();
+    stream.on("data", (chunk) => buffer.push(chunk));
+    stream.on("end", () => resolve(Buffer.concat(buffer)));
+    stream.on("error", (err) => reject(`error converting stream - ${err}`));
+  });
+}
+
+const headersToSkip = ["host", "cookie", "authorization"];
+export function copyHeaders(
+  reqHeaders: IncomingHttpHeaders,
+): IncomingHttpHeaders {
+  const headers: IncomingHttpHeaders = {};
+  for (const headersKey in reqHeaders) {
+    if (!headersToSkip.includes(headersKey.toLowerCase())) {
+      headers[headersKey] = reqHeaders[headersKey];
+    }
+  }
+  return headers;
+}
 
 const handler = async (
   req: NextApiRequest,
@@ -16,24 +41,58 @@ const handler = async (
   const path = Array.isArray(slug) ? slug.join("/") : slug;
 
   const oboToken = await getOboToken(token);
-  console.log("path: ", oboToken);
-  console.log(
-    "proxying request to: http://",
-    process.env.NEXT_AVTALER_API_HOSTNAME ?? "",
-    "/",
-    path,
-  );
   try {
-    await proxyApiRouteRequest({
-      req,
-      res,
-      bearerToken: oboToken,
+    const headers = {
+      ...copyHeaders(req.headers),
+    };
+    if (oboToken) {
+      headers.Authorization = `Bearer ${oboToken}`;
+    }
+    console.log("headers: ", headers);
+
+    const requestOptions: RequestOptions = {
       hostname: process.env.NEXT_AVTALER_API_HOSTNAME ?? "",
-      path: path as string,
-      https: false,
+      port: 80,
+      path,
+      method: req.method,
+      headers,
+    };
+
+    console.log("requestOptions: ", requestOptions);
+
+    const stream = Readable.from(req);
+    const bodyResponse = await stream2buffer(stream);
+    const backendReq = http.request(requestOptions, (proxyRequestResponse) => {
+      console.log("tok imot response: ", proxyRequestResponse.statusCode, ", url: ", proxyRequestResponse.url);
+      if (proxyRequestResponse.statusCode != null) {
+        res.status(proxyRequestResponse.statusCode);
+      }
+      for (const headersKey in proxyRequestResponse.headers) {
+        const header = proxyRequestResponse.headers[headersKey];
+        if (header) {
+          res.setHeader(headersKey, header);
+        }
+      }
+
+      proxyRequestResponse.on("data", (data: unknown) => {
+        res.write(data);
+      });
+      proxyRequestResponse.on("end", () => {
+        res.end();
+      });
     });
+
+    backendReq.on("error", (error) => {
+      console.warn("Error in proxy request:", error);
+      res
+        .status(500)
+        .json({ message: "Error occurred while proxying the request." });
+    });
+
+    backendReq.write(bodyResponse);
+    backendReq.end();
   } catch (e) {
-    console.error("Noe feilet under proxying: " ,e);
+    console.error("Noe feilet under proxying: ", e);
   }
 };
 
