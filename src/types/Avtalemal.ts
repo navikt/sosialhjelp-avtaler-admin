adf17d9a0f73ec6d545445616413b22f389d2940
Martin Almvik <martin.almvik@nav.no>
2024-06-07 10:09:26 +0200

    Legger til replacement map

--- src/types/Avtalemal.ts
+++ src/types/Avtalemal.ts
@@ -3,4 +3,12 @@ export interface Avtalemal {
   dokumentUrl: string;
   publisert: string;
   uuid: string;
+  previewUrl: string;
+  replacementMap: Record<string, Replacement>;
+}
+
+export enum Replacement {
+  KOMMUNENAVN = "KOMMUNENAVN",
+  KOMMUNEORGNR = "KOMMUNEORGNR",
+  DATO = "DATO",
 }
