ca0713f19db15808d6b4d457d14ed8bebccdf4c1
Martin Almvik <martin.almvik@nav.no>
2024-08-28 11:34:27 +0200

    Tar bort dekoratøren

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,5 @@
-import { buildCspHeader } from "@navikt/nav-dekoratoren-moduler/ssr/index.js";
-
-const appDirectives = {
-  "default-src": ["'self'"],
-  "script-src": [
-    "'self'",
-    "'unsafe-eval'",
-    "https://uxsignals-frontend.uxsignals.app.iterate.no",
-  ],
-  "script-src-elem": [
-    "'self'",
-    "https://uxsignals-frontend.uxsignals.app.iterate.no",
-  ],
-  "style-src": ["'self'"],
-  "img-src": ["'self'", "data:"],
-  "font-src": ["'self'", "https://cdn.nav.no"],
-  "worker-src": ["'self'"],
-  "connect-src": ["'self'", "https://*.nav.no", "https://*.uxsignals.com"],
-};
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    const environment =
-      process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT === "prod" ? "prod" : "dev";
-    const cspValue = await buildCspHeader(appDirectives, { env: environment });
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspValue,
-          },
-        ],
-      },
-    ];
-  },
   reactStrictMode: true,
   basePath: "/sosialhjelp/avtaler-admin",
   output: "standalone",
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,51 +3,13 @@ import Document, {
   Head,
   Main,
   NextScript,
-  DocumentContext,
-  DocumentInitialProps,
 } from "next/document";
-import { DecoratorFetchProps } from "@navikt/nav-dekoratoren-moduler";
-import {
-  DecoratorComponents,
-  fetchDecoratorReact,
-} from "@navikt/nav-dekoratoren-moduler/ssr";
 
-interface Props {
-  Decorator: DecoratorComponents;
-}
-
-const decoratorParams = (ctx: DocumentContext): DecoratorFetchProps => ({
-  env: process.env.NEXT_PUBLIC_RUNTIME_ENVIRONMENT as "dev" | "prod",
-  serviceDiscovery: true,
-  params: {
-    simple: false,
-    feedback: false,
-    chatbot: false,
-    shareScreen: false,
-    utilsBackground: "white",
-    logoutUrl: undefined,
-    logoutWarning: true,
-  },
-});
-
-class MyDocument extends Document<Props> {
-  static async getInitialProps(
-    ctx: DocumentContext,
-  ): Promise<DocumentInitialProps & Props> {
-    const initialProps = await Document.getInitialProps(ctx);
-
-    const props = decoratorParams(ctx);
-    const Decorator = await fetchDecoratorReact(props);
-
-    return { ...initialProps, Decorator };
-  }
-
+class MyDocument extends Document {
   render(): React.JSX.Element {
-    const { Decorator } = this.props;
     return (
       <Html>
         <Head>
-          <Decorator.Styles />
           <link
             rel="preload"
             href="https://cdn.nav.no/aksel/fonts/SourceSans3-normal.woff2"
@@ -62,10 +24,7 @@ class MyDocument extends Document<Props> {
           />
         </Head>
         <body>
-          <Decorator.Header />
           <Main />
-          <Decorator.Footer />
-          <Decorator.Scripts />
           <NextScript />
         </body>
       </Html>
